b10c6aeaf1ca93eb72994c130849fafc60e34ba3
Savannah Walters <savwalts@gmail.com>
2021-03-30 12:48:37 -0700

    updated requireUser- still neeed to figure out other areas that might need that functionality

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -114,6 +114,20 @@ usersRouter.post('/login', async(req, res, next) => {
     }
 });
 
+usersRouter.get('/me', requireUser, async (req, res, next) => {
+    try {
+        if (req.headers.authorization) {
+            const token = req.headers.authorization.split(' ')[1];
+            const user = jwt.verify(token, JWT_SECRET);
+            res.send({...user});
+        } else {
+            return next({message: 'You must be logged in to perform this action'});
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 
 
